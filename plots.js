2e2bd644f82675945ce7fbf081c828856124b5ee
feipengyang <feipeng.yang@outlook.com>
2021-12-18 16:43:59 -0600

    Implemented function stubs and event handler.

--- plots.js
+++ plots.js
@@ -2,6 +2,35 @@ console.log("This is plots.js..");
 
 // This code is taken from Dom's office hour section on Dec 11. 
 
+function DrawBarChart(sampleId) {
+    console.log(`DrawBarChart (${sampleId})`);
+}
+
+function DrawBubbleChart(sampleId) {
+    console.log(`DrawBubbleChart (${sampleId})`);
+}
+
+function ShowMetaData(sampleId) {
+    console.log(`ShowMetaData (${sampleId})`);
+}
+
+
+
+function optionChanged(id) {
+    console.log(`optionChanged (${id})`);
+
+    // Display the barChart
+    DrawBarChart(id);
+
+    DrawBubbleChart(id);
+
+    ShowMetaData(id);
+    // Display the bubbleChart
+
+    // Populate demographic infor
+}
+
+
 function InitDashboard()
 {
     console.log("Initializing Dashboard.");
@@ -20,6 +49,12 @@ function InitDashboard()
                 .property("value", sampleId);
 
         });
+
+        let sampleId = sampleNames[0];
+
+        DrawBarChart(sampleId);
+        DrawBubbleChart(sampleId);
+        ShowMetaData(sampleId);
 
 
     });
